398d939070a55c431d9ca62695ca13b536702377
gavin2lee <gavin2lee@163.com>
2016-11-10 14:44:51 +0800

    des

---
+++ src/main/java/com/lachesis/support/auth/encryption/impl/DesEncrypter.java
@@ -0,0 +1,140 @@
+package com.lachesis.support.auth.encryption.impl;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.lachesis.support.auth.encryption.Encrypter;
+
+public class DesEncrypter implements Encrypter {
+	private static final Logger LOG = LoggerFactory.getLogger(DesEncrypter.class);
+	
+	private static final String ENCRYPTION_ALGORITHM_DES = "DES";
+	private static final String ENCODING  = "UTF-8";
+		
+	private String encryptionKey = "LX123456";//TODO configurable here and read from file
+
+	@Override
+	public String encrypt(String plainText) {
+		if(LOG.isDebugEnabled()){
+			LOG.debug(String.format("encrypting %s", plainText));
+		}
+		
+		if(StringUtils.isBlank(plainText)){
+			return null;
+		}
+		
+		return doEncrypt(plainText);
+	}
+
+	@Override
+	public String decrypt(String cryptograph) {
+		if(LOG.isDebugEnabled()){
+			LOG.debug(String.format("decrypting %s", cryptograph));
+		}
+		
+		if(StringUtils.isBlank(cryptograph)){
+			return null;
+		}
+		
+		return doDecrypt(cryptograph);
+	}
+	
+	private String doDecrypt(String cryptograph){
+		byte[] decodedCipherBytes = Base64.decodeBase64(cryptograph);
+		if(decodedCipherBytes == null){
+			LOG.error("decoding failed");
+			return null;
+		}
+		
+		byte[] decryptedBytes;
+		try {
+			decryptedBytes = deDecryptWithDes(decodedCipherBytes, getEncryptionKey());
+		} catch (Exception e) {
+			LOG.error("decryption error.", e);
+			decryptedBytes = null;
+		}
+		
+		if(decryptedBytes == null){
+			LOG.error("decrypting failed");
+			return null;
+		}
+		
+		String plainText = null;
+		try {
+			plainText =  new String(decryptedBytes, ENCODING);
+		} catch (UnsupportedEncodingException e) {
+			LOG.error("error while creating plain text", e);
+		}
+		
+		return plainText;
+	}
+	
+	private byte[] deDecryptWithDes(byte[] cipherBytes, String encryptionKey) throws InvalidKeyException, UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException{
+		SecureRandom random = new SecureRandom();
+		DESKeySpec desKey = new DESKeySpec(encryptionKey.getBytes(ENCODING));
+		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(ENCRYPTION_ALGORITHM_DES);
+		SecretKey securekey = keyFactory.generateSecret(desKey);
+		Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM_DES);
+		cipher.init(Cipher.DECRYPT_MODE, securekey, random);
+		byte[] decryptBytes = cipher.doFinal(cipherBytes);
+		
+		return decryptBytes;
+	}
+	
+	private String doEncrypt(String plainText){
+		byte[] cipherBytes;
+		try {
+			cipherBytes = doEncryptStringWithDes(plainText, getEncryptionKey());
+		} catch (Exception e) {
+			LOG.error("error while encrypting.", e);
+			cipherBytes = null;
+		}
+		
+		if(cipherBytes == null){
+			LOG.warn("encrypting failed");
+			return null;
+		}
+		
+		String encodedCipherString = Base64.encodeBase64String(cipherBytes);
+		
+		if(LOG.isDebugEnabled()){
+			LOG.debug(String.format("encryption result:%s",encodedCipherString));
+		}
+		return encodedCipherString;
+	}
+	
+	private byte[] doEncryptStringWithDes(String plainText, String encrytionKey) throws NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException{
+		SecureRandom random = new SecureRandom();
+		DESKeySpec desKey = new DESKeySpec(encrytionKey.getBytes(ENCODING));
+		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(ENCRYPTION_ALGORITHM_DES);
+		
+		SecretKey securekey = keyFactory.generateSecret(desKey);
+		
+		Cipher cipher = Cipher.getInstance(ENCRYPTION_ALGORITHM_DES);
+		
+		cipher.init(Cipher.ENCRYPT_MODE, securekey, random);
+		
+		byte[] cipherBytes = cipher.doFinal(plainText.getBytes(ENCODING));
+		return cipherBytes;
+	}
+	
+	private String getEncryptionKey(){
+		return encryptionKey;
+	}
+}
---
+++ src/test/java/com/lachesis/support/auth/encryption/impl/DesEncrypterTest.java
@@ -0,0 +1,69 @@
+package com.lachesis.support.auth.encryption.impl;
+
+//import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.lachesis.support.auth.encryption.Encrypter;
+
+public class DesEncrypterTest {
+	Encrypter encrypter;
+
+	@Before
+	public void setUp() throws Exception {
+		encrypter = new DesEncrypter();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Ignore
+	@Test
+	public void testEncrypt() {
+		String plainText = "username|10.2.3.74|147666666";
+
+		String cipherText = encrypter.encrypt(plainText);
+
+		Assert.assertNotNull(cipherText);
+
+		// "/NQ5ultMnf9deFRuHmyGkxnx8QrEfKp2c4Vq4yT9Nos="
+	}
+
+	@Ignore
+	@Test
+	public void testDecrypt() {
+		String token = "/NQ5ultMnf9deFRuHmyGkxnx8QrEfKp2c4Vq4yT9Nos=";
+
+		String plainText = encrypter.decrypt(token);
+
+		Assert.assertNotNull(plainText);
+		// "username|10.2.3.74|147666666"
+	}
+
+	@Test
+	public void testEncryptAndDecrypt() {
+		long startTime = System.currentTimeMillis();
+		String plainText = "username|10.2.3.74|1478760076830";
+
+		String cipherText = encrypter.encrypt(plainText);
+		
+		Assert.assertNotNull(cipherText);
+		
+		String decryptedText = encrypter.decrypt(cipherText);
+		Assert.assertNotNull(decryptedText);
+		
+		Assert.assertEquals("compare plain text with cipher text", plainText, decryptedText);
+		
+		long endTime = System.currentTimeMillis();
+		
+		long timeElapse = (endTime - startTime);
+		
+		Assert.assertTrue("elapsed time shoud less than 150 milliseconds", timeElapse < 150);
+	}
+
+}
