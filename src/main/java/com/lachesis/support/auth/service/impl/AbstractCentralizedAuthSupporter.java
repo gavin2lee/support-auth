898c35c3711c7876df5f142ace9da8fe0426e7f2
gavin2lee <gavin2lee@163.com>
2016-11-10 15:42:17 +0800

    ab

--- src/main/java/com/lachesis/support/auth/service/impl/AbstractCentralizedAuthSupporter.java
+++ src/main/java/com/lachesis/support/auth/service/impl/AbstractCentralizedAuthSupporter.java
@@ -7,17 +7,17 @@ import org.slf4j.LoggerFactory;
 import com.lachesis.support.auth.model.UserDetails;
 import com.lachesis.support.auth.service.CentralizedAuthSupporter;
 
-public abstract class AbstractCentralizedAuthSupporter implements CentralizedAuthSupporter{
+public abstract class AbstractCentralizedAuthSupporter implements CentralizedAuthSupporter {
 	private static final Logger LOG = LoggerFactory.getLogger(AbstractCentralizedAuthSupporter.class);
-	
+
 	public String generateToken(String userid, String password, String terminalIpAddress) {
 		if (LOG.isDebugEnabled()) {
 			LOG.debug(String.format("generating token for [userid:%s,ip:%s]", userid, terminalIpAddress));
 		}
 
-		String passwordMask = (StringUtils.isBlank(password) ? null : "***");
+		String passwordMask = (isBlank(password) ? null : "***");
 
-		if (StringUtils.isBlank(userid) || StringUtils.isBlank(password) || StringUtils.isBlank(terminalIpAddress)) {
+		if (isBlank(userid) || isBlank(password) || isBlank(terminalIpAddress)) {
 			LOG.error(String.format("generating token failed for [userid:%s,password:%s,ip:%s]", userid, passwordMask,
 					terminalIpAddress));
 			throw new RuntimeException("generating token failed");
@@ -27,29 +27,37 @@ public abstract class AbstractCentralizedAuthSupporter implements CentralizedAut
 	}
 
 	public UserDetails authenticate(String token, String terminalIpAddress) {
-		if(LOG.isDebugEnabled()){
-			LOG.debug(String.format("authenticating token for [token:%s,ip:%s]", token,terminalIpAddress));
+		if (LOG.isDebugEnabled()) {
+			LOG.debug(String.format("authenticating token for [token:%s,ip:%s]", token, terminalIpAddress));
 		}
-		
-		if(StringUtils.isBlank(token) || StringUtils.isBlank(terminalIpAddress)){
-			LOG.error(String.format("authenticating failed for [token:%s,ip:%s]", token,terminalIpAddress));
+
+		if (isBlank(token) || isBlank(terminalIpAddress)) {
+			LOG.error(String.format("authenticating failed for [token:%s,ip:%s]", token, terminalIpAddress));
 			return null;
 		}
-		
+
 		return doAuthenticate(token, terminalIpAddress);
 	}
-	
+
 	@Override
 	public void dismissToken(String token) {
-		if(LOG.isInfoEnabled()){
+		if (LOG.isInfoEnabled()) {
 			LOG.info(String.format("dismissing token:%s", token));
 		}
 		
-		//TODO
-		
+		if(isBlank(token)){
+			return;
+		}
+
+		// TODO
+
 	}
 	
+	protected boolean isBlank(String s){
+		return StringUtils.isBlank(s);
+	}
+
 	protected abstract String doGenerateToken(String userid, String password, String terminalIpAddress);
-	
+
 	protected abstract UserDetails doAuthenticate(String token, String terminalIpAddress);
 }
