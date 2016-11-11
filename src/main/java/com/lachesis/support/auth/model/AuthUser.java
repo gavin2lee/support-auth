71924028f362737ee52ea769e25595fb0233901a
gavin2lee <gavin2lee@163.com>
2016-11-11 16:36:23 +0800

    auth

--- src/main/java/com/lachesis/support/auth/model/AuthUser.java
+++ src/main/java/com/lachesis/support/auth/model/AuthUser.java
@@ -1,5 +1,40 @@
 package com.lachesis.support.auth.model;
 
 public class AuthUser {
+	private Long id;
+	private String username;
+	private String userCode;
+	private String password;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getUserCode() {
+		return userCode;
+	}
+
+	public void setUserCode(String userCode) {
+		this.userCode = userCode;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
--- src/main/java/com/lachesis/support/auth/repository/AuthUserRepository.java
+++ src/main/java/com/lachesis/support/auth/repository/AuthUserRepository.java
@@ -1,7 +1,14 @@
 package com.lachesis.support.auth.repository;
 
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 import com.lachesis.support.auth.model.AuthUser;
 
 public interface AuthUserRepository {
-	AuthUser findByUserid(String userid);
+	@Select("SELECT SEQ_ID as id,LOGIN_NAME as username,USER_CODE as userCode,PASSWORD as password "
+			+"FROM SYS_USER "
+			+"WHERE LOGIN_NAME=#{userid}"
+			+"AND STATUS='01'")
+	AuthUser findByUserid(@Param("userid")String userid);
 }
