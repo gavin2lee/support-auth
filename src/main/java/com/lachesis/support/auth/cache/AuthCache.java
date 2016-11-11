13c18e58a486252eb799b2448b40ad19dc084a58
gavin2lee <gavin2lee@163.com>
2016-11-11 17:48:44 +0800

    cache

--- src/main/java/com/lachesis/support/auth/cache/AuthCache.java
+++ src/main/java/com/lachesis/support/auth/cache/AuthCache.java
@@ -2,6 +2,7 @@ package com.lachesis.support.auth.cache;
 
 public interface AuthCache {
 	void put(String key,Object value);
+	void update(String key,Object value);
 	Object get(String key);
 	void remove(String key);
 }
