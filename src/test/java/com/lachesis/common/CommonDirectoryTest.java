5f5e4a9735bf6ce5a3e3ddb25164ddd6b4eaf068
gavin2lee <gavin2lee@163.com>
2016-11-14 22:51:32 +0800

    log

--- src/test/java/com/lachesis/common/CommonDirectoryTest.java
+++ src/test/java/com/lachesis/common/CommonDirectoryTest.java
@@ -2,8 +2,11 @@ package com.lachesis.common;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CommonDirectoryTest {
+	static final Logger LOG = LoggerFactory.getLogger(CommonDirectoryTest.class);
 
 	@Before
 	public void setUp() throws Exception {
@@ -11,8 +14,8 @@ public class CommonDirectoryTest {
 
 	@Test
 	public void testUserHome() {
-		System.out.println("user.home:"+System.getProperty("user.home"));
-		System.out.println("java.home:"+System.getProperty("java.home"));
+		LOG.info("user.home:"+System.getProperty("user.home"));
+		LOG.info("java.home:"+System.getProperty("java.home"));
 	}
 
 }
